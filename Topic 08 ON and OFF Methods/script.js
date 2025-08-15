fd1b5e9f49f78e55ace37909eb319b931ca43b5d
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-08-15 17:39:10 +0500

    Added T 08 file

---
+++ Topic 08 ON and OFF Methods/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    // $('#box').on('mouseenter mouseleave', function() {
+    //     $(this).toggleClass('addBgRed');
+    // });   
+
+    $('#myBtn').on({
+        'click': function () {
+            $('#box').css('background-color', 'red');
+        }
+    });
+
+    $('#box').on({
+        'mouseenter': function () {
+            $(this).css('background-color', 'blue');
+        },
+        'mouseleave': function () {
+            $(this).css('background-color', 'green');
+        }
+    });
+
+    $('#newBtn').click(function () {
+        $('#myBtn').off({
+            'click': function(){
+                $(this).css('color', 'yellow');
+            }
+        });
+    });
+
+});
